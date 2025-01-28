2dd84485b2eee01b3db4996dd096bbf87a56d1c8
Sunny Patel <sp@Sunnys-MacBook-Air.local>
2025-01-28 21:58:01 +1100

    adding spring boot starter file

---
+++ src/main/java/com/rm/test/FinderApp/FinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.rm.test.FinderApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rm/test/FinderApp/FinderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.test.FinderApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
